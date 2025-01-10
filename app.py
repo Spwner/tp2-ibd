5180ab7ebdd09f8434005040f83babf7dcc28597
Spwner <henrique.lge175@gmail.com>
2025-01-10 09:51:58 -0300

    mudando todos os nomes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 # Importando os dados
 # Carregar os arquivos de uma vez
 dados_candidatos_partes = [
-    pd.read_csv(f'consulta_cand_2024_BRASIL.csv.part{i:02d}', delimiter=';', encoding='latin1') 
+    pd.read_csv(f'consulta_cand_2024_BRASIL_{i:02d}.csv', delimiter=';', encoding='latin1') 
     for i in range(1, 3)
 ]
 
@@ -16,7 +16,7 @@ dados_candidatos = pd.concat(dados_candidatos_partes, ignore_index=True)
 
 # Para dados de bens
 dados_bens_partes = [
-    pd.read_csv(f'bem_candidato_2024_BRASIL.csv.part{i:02d}', delimiter=';', encoding='latin1')
+    pd.read_csv(f'bem_candidato_2024_BRASIL_{i:02d}.csv', delimiter=';', encoding='latin1')
     for i in range(1, 3)
 ]
 
@@ -25,7 +25,7 @@ dados_bens = pd.concat(dados_bens_partes, ignore_index=True)
 
 # Para dados de redes sociais
 dados_redes_partes = [
-    pd.read_csv(f'rede_social_candidato_BRASIL.csv.part{i}', delimiter=';', encoding='latin1')
+    pd.read_csv(f'rede_social_candidato_2024_BRASIL_{i:02d}.csv', delimiter=';', encoding='latin1')
     for i in range(1, 3)
 ]
 
